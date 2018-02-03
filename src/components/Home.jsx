63b2081389ea82c85292c70e4dd2d4e32018b14d
felimadu <WaitNow>
2018-02-03 17:47:13 -0500

    Added the code for a scrollable side bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,6 +17,59 @@ const sendername = 'Sender Name';
 const blockStackId = 'felix.id';
 const status = 'This us just a sample status for a start. I am going high right now so that I can leave';
 const message_text = 'this is my message to the world, and it is this that I am writing because I feel like it and I';
+const time_sent = new Date();
+
+const MESSAGES = [
+    {id: 1, sender: username, message: message_text, time_received: time_sent.getDate()},
+    {id: 2, sender: username, message: message_text, time_received: time_sent.getDate() + 1},
+    {id: 3, sender: username, message: message_text, time_received: time_sent.getDate() + 2},
+    {id: 4, sender: username, message: message_text, time_received: time_sent.getDate() + 3},
+    {id: 5, sender: username, message: message_text, time_received: time_sent.getDate() + 4},
+    {id: 6, sender: username, message: message_text, time_received: time_sent.getDate() + 5},
+    {id: 7, sender: username, message: message_text, time_received: time_sent.getDate() + 6},
+    {id: 8, sender: username, message: message_text, time_received: time_sent.getDate() + 7}
+  ];
+
+
+class MessageTile extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}; 
+    }
+
+    render() {
+
+        return (
+
+        <table className="table table-hover">
+            <tbody>
+            {
+                this.props.messages.map((message) => {  
+                    return (
+                        <tr><td className="table-row">
+                        <div className="sidebar-message-tile" key={message.id}> 
+                            <div className="other-user-pic"> 
+                                <img src={avatarFallbackImage} className="img-rounded message-pic"/>
+                            </div>
+
+                            <div className="message-preview">
+                                <p> 
+                                    <span className="sender-name">{message.sender}</span>
+                                    <span className="message-time">{message.time_received}</span> 
+                                </p>
+                                    <p className="message-snippet">{message.message}</p>
+                            </div>
+                        </div>
+                        </td></tr>
+                    );
+                })
+            }       
+            </tbody>
+        </table>
+        );
+    }
+}
 
 export class Home extends Component {
     
@@ -55,17 +108,8 @@ export class Home extends Component {
                                 </form>
                             </div>
 
-                            <div className="messages-sidebar"> 
-                                <div className="sidebar-message-tile"> 
-                                    <div className="other-user-pic">
-                                        <img src={avatarFallbackImage} className="img-rounded message-pic"/>
-                                    </div>
-
-                                    <div className="message-preview">
-                                        <p className="font-weight-bold sender-name">{sendername}</p>
-                                        <p className="message-snippet">{message_text}</p>
-                                    </div>
-                                </div>
+                            <div className="messages-sidebar">
+                                <MessageTile messages={MESSAGES}/>
                             </div>
                         </div>
                     </div>
