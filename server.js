d66efa8596b55cbd501fa256c10a0653258a1a5e
Avthar Sewrathan <avthar.s@gmail.com>
2018-02-14 11:20:20 -0500

    cors added to server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
+var cors = require('cors');
 const app = express();
 
+app.use(cors())
 app.use(express.static('./public/build'));
 
 app.get('*', (req, res) => {
